5e87613b8c6e88087918e02ac93039ba1e2b2cee
Luka Drezga <ldrezga@student.gmail.com>
2022-03-15 21:44:07 +0100

    Promjena forme za unos villa u admin

--- vialux/src/views/Admin.vue
+++ vialux/src/views/Admin.vue
@@ -1,5 +1,95 @@
 <template>
   <div class="about">
-    <h1>Ovo je stranica za admina u kojoj će on dodavati ville na stranicu</h1>
+    <h1>Ovdje će se nalaziti ville u Hrvatskoj.</h1>
+    <section class="content-inside-grey">
+      <div class="container">
+        <div class="row">
+          <div class="col-10">
+            <form @submit.prevent="postNewImage" class="mb-5">
+              <div class="form-group">
+                <croppa
+                  :width="400"
+                  :height="400"
+                  placeholder="Učitaj sliku..."
+                  v-model="imageReference"
+                ></croppa>
+              </div>
+              <div class="form-group">
+                <label for="imageDescription">Description</label>
+                <input
+                  v-model="newImageDescription"
+                  type="text"
+                  class="form-control ml-2"
+                  placeholder="Enter the image description"
+                  id="imageDescription"
+                />
+              </div>
+              <button type="submit" class="btn btn-primary ml-2">
+                Post image
+              </button>
+            </form>
+          </div>
+          <div class="col-2"></div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import store from "@/store";
+import { db, storage } from "@/firebase";
+import { collection, addDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+
+export default {
+  name: "Home",
+  data: function () {
+    return {
+      store,
+      newImageDescription: "",
+      newImageUrl: "",
+      imageReference: null,
+    };
+  },
+  methods: {
+    postNewImage() {
+      const imageDescription = this.newImageDescription;
+
+      this.imageReference.generateBlob((blobData) => {
+        const imageName = store.currentUser + "_" + Date.now() + ".png";
+        const storageRef = ref(storage);
+        const imageRef = ref(
+          storageRef,
+          "slike_villa/" + blobData.name + imageName
+        );
+
+        uploadBytes(imageRef, blobData)
+          .then((snapshot) => {
+            getDownloadURL(ref(storage, imageRef))
+              .then((url) => {
+                console.log("Javni link", url);
+                var docRef = addDoc(collection(db, "ville"), {
+                  url: url,
+                  desc: imageDescription,
+                  email: store.currentUser,
+                });
+                console.log("Spremljeno", docRef);
+              })
+              .catch((e) => {
+                console.error(e);
+              });
+            console.log("Uploaded a file");
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+      });
+    },
+  },
+  components: {
+    store,
+  },
+};
+</script>
--- vialux/src/views/Ville.vue
+++ vialux/src/views/Ville.vue
@@ -5,29 +5,6 @@
       <div class="container">
         <div class="row">
           <div class="col-10">
-            <form @submit.prevent="postNewImage" class="mb-5">
-              <div class="form-group">
-                <croppa
-                  :width="400"
-                  :height="400"
-                  placeholder="Učitaj sliku..."
-                  v-model="imageReference"
-                ></croppa>
-              </div>
-              <div class="form-group">
-                <label for="imageDescription">Description</label>
-                <input
-                  v-model="newImageDescription"
-                  type="text"
-                  class="form-control ml-2"
-                  placeholder="Enter the image description"
-                  id="imageDescription"
-                />
-              </div>
-              <button type="submit" class="btn btn-primary ml-2">
-                Post image
-              </button>
-            </form>
             <Kartaville
               v-for="card in filteredCardes"
               :key="card.url"
@@ -45,21 +22,8 @@
 // @ is an alias to /src
 import Kartaville from "@/components/Kartaville.vue";
 import store from "@/store";
-import { db, storage } from "@/firebase";
-import { collection, addDoc, getDocs } from "firebase/firestore";
-import { ref, uploadBytes } from "firebase/storage";
-import { getDatabase, push, set, ref as dbRef } from "firebase/database";
-
-//cardes = [
-//  {
-//    url: "https://croatia-exclusive.com/storage/app/uploads/public/607/d29/a1a/607d29a1a063e381169397.jpg",
-//    description: "Villa Frida",
-//  },
-//  {
-//    url: "https://www.myistria.com/UserDocsImages/app/objekti/1679/slika_hd/21012022091409_Villa-near-Umag-Villa-Stancija-Baracija%2079.jpg",
-//    description: "Villa Stancija Baracija",
-//  },
-//];
+import { db } from "@/firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 export default {
   name: "Home",
@@ -82,6 +46,7 @@ export default {
       const colRef = collection(db, "ville");
 
       getDocs(colRef).then((snapshot) => {
+        this.cardes = [];
         snapshot.docs.forEach((doc) => {
           const data = doc.data();
           this.cardes.push({
@@ -90,46 +55,6 @@ export default {
           });
         });
       });
-    },
-    postNewImage() {
-      console.log("Ok");
-
-      this.imageReference.generateBlob((blobData) => {
-        console.log(blobData);
-
-        let imageName = store.currentUser + "_" + Date.now() + ".png";
-        const storageRef = ref(storage, "files/" + files.name);
-
-        const database = getDatabase();
-        const databaseReference = dbRef(database, "files");
-
-        uploadBytes(storageRef, files).then(function (snapshot) {
-          var newFileRef = push(databaseReference);
-
-          set(newFileRef, {
-            name: files.name,
-          });
-        });
-      });
-
-      return;
-
-      const imageDescription = this.newImageDescription;
-
-      try {
-        const docRef = addDoc(collection(db, "ville"), {
-          url: imageUrl,
-          desc: imageDescription,
-          email: store.currentUser,
-        });
-        console.log("Spremljeno", docRef);
-        this.newImageDescription = "";
-        this.newImageUrl = "";
-
-        this.getPosts();
-      } catch (e) {
-        console.error("Pogreška", e);
-      }
     },
   },
   computed: {
