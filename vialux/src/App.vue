96dd1162ae7ca262c7f97b6ff45d2b79aca49591
Luka Drezga <ldrezga@student.gmail.com>
2022-04-06 12:59:59 +0200

    Rad buttona u responzivi

--- vialux/src/App.vue
+++ vialux/src/App.vue
@@ -1,58 +1,60 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-light">
-      <a class="navbar-brand" href="#">
-        <img
-          src="@/assets/vialux.png"
-          alt=""
-          height="50"
-          class="d-inline-block align-text-top"
-        />
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarToggler"
-        aria-controls="navbarToggler"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarToggler">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link to="/" class="nav-link">Početna</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/ville" class="nav-link">Ville</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/kontakt" class="nav-link">Kontakt</router-link>
-          </li>
-          <li v-if="!store.currentUser" class="nav-item">
-            <router-link to="/login" class="nav-link">Login</router-link>
-          </li>
-          <li v-if="!store.currentUser" class="nav-item">
-            <router-link to="/signup" class="nav-link">Signup</router-link>
-          </li>
-          <li v-if="store.currentUser" class="nav-item">
-            <a href="#" @click="logout()" class="nav-link">Logout</a>
-          </li>
-          <li v-if="store.currentUser" class="nav-item">
-            <router-link to="/admin" class="nav-link">Admin</router-link>
-          </li>
-        </ul>
-        <form class="d-flex">
-          <input
-            v-model="store.searchTerm"
-            class="form-control me-2"
-            type="search"
-            placeholder="Pretraga"
-            aria-label="Pretraga"
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            src="@/assets/vialux.png"
+            alt=""
+            height="50"
+            class="d-inline-block align-text-top"
           />
-        </form>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarToggler"
+          aria-controls="navbarToggler"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarToggler">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Početna</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/ville" class="nav-link">Ville</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/kontakt" class="nav-link">Kontakt</router-link>
+            </li>
+            <li v-if="!store.currentUser" class="nav-item">
+              <router-link to="/login" class="nav-link">Login</router-link>
+            </li>
+            <li v-if="!store.currentUser" class="nav-item">
+              <router-link to="/signup" class="nav-link">Signup</router-link>
+            </li>
+            <li v-if="store.currentUser" class="nav-item">
+              <a href="#" @click="logout()" class="nav-link">Logout</a>
+            </li>
+            <li v-if="store.currentUser" class="nav-item">
+              <router-link to="/admin" class="nav-link">Admin</router-link>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input
+              v-model="store.searchTerm"
+              class="form-control me-2"
+              type="search"
+              placeholder="Pretraga"
+              aria-label="Pretraga"
+            />
+          </form>
+        </div>
       </div>
     </nav>
     <div class="conatiner">
